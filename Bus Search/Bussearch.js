de578fefd54669592a3899bb059e58ec2e35d41a
vignesh k <127012409+vignesh2607@users.noreply.github.com>
2023-03-24 14:18:21 +0530

    Add files via upload

---
+++ Bus Search/Bussearch.js
@@ -0,0 +1,18 @@
+function booking(){
+    let book=document.getElementById("busbook").value;
+    if(book=='Bangalore'){
+        window.open
+    }
+    elseif (book=='Hydrabad')
+    {
+        window.location.href="";
+    }
+    elseif (book=='goa')
+    {
+        window.location.href="";
+    }
+    elseif (book=='kerala')
+    {
+        window.location.href="";
+    }
+}
